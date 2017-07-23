f18624b86007e6445797e639a505811b845d74ce
Micalgenus <micalgenus@gmail.com>
2017-07-24 01:16:03 +0900

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,7 +4,7 @@ if [ -z "$(ls -A /usr/share/meteor)" ]; then
 	cd /usr/share
 	chmod o+w /usr/share
 	chown meteor:meteor -R /usr/share/meteor
-	su meteor -c "/home/meteor/.meteor/meteor create --full meteor"
+	su meteor -c "/home/meteor/.meteor/meteor create meteor"
 
 	/package.sh /usr/share/meteor/package.json $MONGO_HOST $MONGO_PORT $MONGO_DB
 
